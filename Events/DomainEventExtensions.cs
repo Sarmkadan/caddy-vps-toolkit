4fa38a86b3fe083e20ef667644b4b32d27ea029d
Vladyslav Zaiets <rutova2@gmail.com>
2024-05-07 19:27:19 +0300

    more DomainEvent extension methods

--- Events/DomainEventExtensions.cs
+++ Events/DomainEventExtensions.cs
@@ -3,7 +3,8 @@ using System;
 namespace CaddyVpsToolkit.Events
 {
     /// <summary>
-    /// Extension methods that add convenient helpers for <see cref="DomainEvent"/>.
+    /// Provides extension methods for <see cref="DomainEvent"/> that offer convenient operations
+    /// for working with domain events.
     /// </summary>
     public static class DomainEventExtensions
     {
@@ -12,22 +13,22 @@ namespace CaddyVpsToolkit.Events
         /// </summary>
         /// <param name="domainEvent">The event instance.</param>
         /// <returns>A <see cref="TimeSpan"/> representing how long ago the event happened.</returns>
-        /// <exception cref="ArgumentNullException">If <paramref name="domainEvent"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="domainEvent"/> is <c>null</c>.</exception>
         public static TimeSpan GetAge(this DomainEvent domainEvent)
         {
-            if (domainEvent == null) throw new ArgumentNullException(nameof(domainEvent));
+            ArgumentNullException.ThrowIfNull(domainEvent);
             return DateTime.UtcNow - domainEvent.OccurredAt;
         }
 
         /// <summary>
-        /// Returns a detailed, single‑line string representation of the event.
+        /// Returns a detailed, single-line string representation of the event.
         /// </summary>
         /// <param name="domainEvent">The event instance.</param>
         /// <returns>A formatted string containing the key properties.</returns>
-        /// <exception cref="ArgumentNullException">If <paramref name="domainEvent"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="domainEvent"/> is <c>null</c>.</exception>
         public static string ToDetailedString(this DomainEvent domainEvent)
         {
-            if (domainEvent == null) throw new ArgumentNullException(nameof(domainEvent));
+            ArgumentNullException.ThrowIfNull(domainEvent);
             return $"EventId: {domainEvent.EventId}, AggregateId: {domainEvent.AggregateId}, OccurredAt: {domainEvent.OccurredAt:O}";
         }
 
@@ -36,11 +37,11 @@ namespace CaddyVpsToolkit.Events
         /// </summary>
         /// <param name="domainEvent">The event instance.</param>
         /// <param name="maxAge">The maximum allowed age.</param>
-        /// <returns><c>true</c> if the event is newer than <paramref name="maxAge"/>; otherwise, <c>false</c>.</returns>
-        /// <exception cref="ArgumentNullException">If <paramref name="domainEvent"/> is <c>null</c>.</exception>
+        /// <returns><see langword="true"/> if the event is newer than <paramref name="maxAge"/>; otherwise, <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="domainEvent"/> is <c>null</c>.</exception>
         public static bool IsRecent(this DomainEvent domainEvent, TimeSpan maxAge)
         {
-            if (domainEvent == null) throw new ArgumentNullException(nameof(domainEvent));
+            ArgumentNullException.ThrowIfNull(domainEvent);
             return domainEvent.GetAge() <= maxAge;
         }
 
@@ -49,12 +50,12 @@ namespace CaddyVpsToolkit.Events
         /// </summary>
         /// <param name="domainEvent">The first event.</param>
         /// <param name="other">The second event to compare with.</param>
-        /// <returns><c>true</c> if both events have the same <c>AggregateId</c>; otherwise, <c>false</c>.</returns>
-        /// <exception cref="ArgumentNullException">If either argument is <c>null</c>.</exception>
+        /// <returns><see langword="true"/> if both events have the same <c>AggregateId</c>; otherwise, <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentNullException">Either <paramref name="domainEvent"/> or <paramref name="other"/> is <c>null</c>.</exception>
         public static bool HasSameAggregate(this DomainEvent domainEvent, DomainEvent other)
         {
-            if (domainEvent == null) throw new ArgumentNullException(nameof(domainEvent));
-            if (other == null) throw new ArgumentNullException(nameof(other));
+            ArgumentNullException.ThrowIfNull(domainEvent);
+            ArgumentNullException.ThrowIfNull(other);
             return string.Equals(domainEvent.AggregateId, other.AggregateId, StringComparison.Ordinal);
         }
     }
