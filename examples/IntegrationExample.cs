656881b8438216e9c1e8474b98862d62c390cc4a
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-05 07:34:07 +0000

    refactor: code cleanup and modernization

--- examples/IntegrationExample.cs
+++ examples/IntegrationExample.cs
@@ -1,5 +1,4 @@
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using CaddyVpsToolkit.Services;
 using CaddyVpsToolkit.Data;
 using CaddyVpsToolkit.Notifications;
