c0425f230835b0d1e22b773ec79ebc7ca5928e3a
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-11 14:56:10 +0000

    refactor(utilities): tighten guard clauses and docs in CollectionExtensions

--- Utilities/CollectionExtensions.cs
+++ Utilities/CollectionExtensions.cs
@@ -17,16 +17,23 @@ namespace CaddyVpsToolkit.Utilities
     public static class CollectionExtensions
     {
         /// <summary>
-        /// Safely get item at index or return default
+        /// Safely get item at index or return default value
         /// </summary>
+        /// <param name="list">The list to access</param>
+        /// <param name="index">The zero-based index to retrieve</param>
+        /// <param name="defaultValue">The default value to return if index is out of bounds</param>
+        /// <returns>The item at the specified index, or defaultValue if index is invalid</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="list"/> is null</exception>
         public static T SafeGet<T>(this IList<T> list, int index, T defaultValue = default)
         {
-            return (list is not null && index >= 0 && index < list.Count) ? list[index] : defaultValue;
+            ArgumentNullException.ThrowIfNull(list);
+            return index >= 0 && index < list.Count ? list[index] : defaultValue;
         }
 
         /// <summary>
         /// Check if collection is null or empty
         /// </summary>
+        /// <returns>True if collection is null or empty; otherwise false</returns>
         public static bool IsNullOrEmpty<T>(this IEnumerable<T> collection)
         {
             return collection is null || !collection.Any();
@@ -35,20 +42,27 @@ namespace CaddyVpsToolkit.Utilities
         /// <summary>
         /// Get first or default with null safety
         /// </summary>
-        public static T FirstOrDefault<T>(this IEnumerable<T> collection)
+        /// <param name="collection">The collection to search</param>
+        /// <returns>The first element or default value</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="collection"/> is null</exception>
+        public static T? FirstOrDefault<T>(this IEnumerable<T> collection)
         {
-            return collection is null ? default! : collection.FirstOrDefault()!;
+            ArgumentNullException.ThrowIfNull(collection);
+            return collection.FirstOrDefault();
         }
 
         /// <summary>
         /// Split collection into batches of specified size
         /// </summary>
+        /// <param name="collection">The collection to batch</param>
+        /// <param name="batchSize">The size of each batch (must be positive)</param>
+        /// <returns>List of batches, each containing up to batchSize elements</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="collection"/> is null</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="batchSize"/> is not positive</exception>
         public static List<List<T>> Batch<T>(this IEnumerable<T> collection, int batchSize)
         {
-            if (collection is null)
-                throw new ArgumentNullException(nameof(collection));
-            if (batchSize <= 0)
-                throw new ArgumentException("Batch size must be positive", nameof(batchSize));
+            ArgumentNullException.ThrowIfNull(collection);
+            ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(batchSize, 0);
 
             var batches = new List<List<T>>();
             var batch = new List<T>();
@@ -72,13 +86,20 @@ namespace CaddyVpsToolkit.Utilities
         /// <summary>
         /// Partition collection into two groups based on predicate
         /// </summary>
+        /// <param name="collection">The collection to partition</param>
+        /// <param name="predicate">The function to test each element</param>
+        /// <returns>A tuple containing matching and not matching elements</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="collection"/> or <paramref name="predicate"/> is null</exception>
         public static (List<T> matching, List<T> notMatching) Partition<T>(
             this IEnumerable<T> collection, Func<T, bool> predicate)
         {
+            ArgumentNullException.ThrowIfNull(collection);
+            ArgumentNullException.ThrowIfNull(predicate);
+
             var matching = new List<T>();
             var notMatching = new List<T>();
 
-            foreach (var item in collection ?? Enumerable.Empty<T>())
+            foreach (var item in collection)
             {
                 if (predicate(item))
                     matching.Add(item);
@@ -92,21 +113,32 @@ namespace CaddyVpsToolkit.Utilities
         /// <summary>
         /// Convert dictionary to list of KeyValuePair with order preservation
         /// </summary>
+        /// <param name="dict">The dictionary to convert</param>
+        /// <returns>List of key-value tuples in dictionary order</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="dict"/> is null</exception>
         public static List<(K key, V value)> ToTupleList<K, V>(this Dictionary<K, V> dict)
         {
-            return dict?.Select(kvp => (kvp.Key, kvp.Value)).ToList() ?? new();
+            ArgumentNullException.ThrowIfNull(dict);
+            return dict.Select(kvp => (kvp.Key, kvp.Value)).ToList();
         }
 
         /// <summary>
         /// Get intersection of multiple collections
         /// </summary>
+        /// <param name="collections">The collections to intersect</param>
+        /// <returns>List containing elements present in all collections</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="collections"/> is null</exception>
         public static List<T> IntersectAll<T>(this IEnumerable<IEnumerable<T>> collections)
         {
+            ArgumentNullException.ThrowIfNull(collections);
+
             if (collections.IsNullOrEmpty())
                 return new();
 
-            var enumerator = collections.GetEnumerator();
-            enumerator.MoveNext();
+            using var enumerator = collections.GetEnumerator();
+            if (!enumerator.MoveNext())
+                return new();
+
             var result = enumerator.Current.ToHashSet();
 
             while (enumerator.MoveNext())
@@ -118,8 +150,14 @@ namespace CaddyVpsToolkit.Utilities
         /// <summary>
         /// Remove items matching predicate from collection
         /// </summary>
+        /// <param name="list">The list to modify</param>
+        /// <param name="predicate">The function to test each element</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="list"/> or <paramref name="predicate"/> is null</exception>
         public static void RemoveWhere<T>(this List<T> list, Func<T, bool> predicate)
         {
+            ArgumentNullException.ThrowIfNull(list);
+            ArgumentNullException.ThrowIfNull(predicate);
+
             for (int i = list.Count - 1; i >= 0; i--)
             {
                 if (predicate(list[i]))
@@ -130,9 +168,15 @@ namespace CaddyVpsToolkit.Utilities
         /// <summary>
         /// Add range of items to collection if they don't already exist
         /// </summary>
+        /// <param name="list">The list to modify</param>
+        /// <param name="items">The items to add</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="list"/> or <paramref name="items"/> is null</exception>
         public static void AddRangeIfNotExists<T>(this List<T> list, IEnumerable<T> items)
         {
-            foreach (var item in items ?? Enumerable.Empty<T>())
+            ArgumentNullException.ThrowIfNull(list);
+            ArgumentNullException.ThrowIfNull(items);
+
+            foreach (var item in items)
             {
                 if (!list.Contains(item))
                     list.Add(item);
@@ -142,9 +186,14 @@ namespace CaddyVpsToolkit.Utilities
         /// <summary>
         /// Shuffle collection using Fisher-Yates algorithm
         /// </summary>
+        /// <param name="collection">The collection to shuffle</param>
+        /// <returns>New list with elements in random order</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="collection"/> is null</exception>
         public static List<T> Shuffle<T>(this IEnumerable<T> collection)
         {
-            var list = collection?.ToList() ?? new();
+            ArgumentNullException.ThrowIfNull(collection);
+
+            var list = collection.ToList();
             var random = new Random();
 
             for (int i = list.Count - 1; i > 0; i--)
