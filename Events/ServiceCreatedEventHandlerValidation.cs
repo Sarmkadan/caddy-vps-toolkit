eb3cdac89338f3e22ee3587e53c5e7b12f123426
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 06:53:41 +0300

    refactor(events): cleanup in ServiceCreatedEventHandlerValidation

--- Events/ServiceCreatedEventHandlerValidation.cs
+++ Events/ServiceCreatedEventHandlerValidation.cs
@@ -2,10 +2,11 @@
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
-// =============================================================================
+// =====================================================================
 
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace CaddyVpsToolkit.Events
 {
@@ -14,12 +15,20 @@ namespace CaddyVpsToolkit.Events
     /// </summary>
     public static class ServiceCreatedEventHandlerValidation
     {
+        private static readonly FieldInfo? _loggerField = typeof(ServiceCreatedEventHandler).GetField(
+            "_logger",
+            BindingFlags.NonPublic | BindingFlags.Instance);
+
+        private static readonly FieldInfo? _webhookHandlerField = typeof(ServiceCreatedEventHandler).GetField(
+            "_webhookHandler",
+            BindingFlags.NonPublic | BindingFlags.Instance);
+
         /// <summary>
         /// Validates a <see cref="ServiceCreatedEventHandler"/> instance
         /// </summary>
-        /// <param name="value">The handler to validate</param>
-        /// <returns>A list of validation errors; empty if valid</returns>
-        /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null</exception>
+        /// <param name="value">The handler to validate. Cannot be null.</param>
+        /// <returns>A list of validation errors; empty if valid.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null.</exception>
         public static IReadOnlyList<string> Validate(this ServiceCreatedEventHandler value)
         {
             ArgumentNullException.ThrowIfNull(value);
@@ -27,34 +36,26 @@ namespace CaddyVpsToolkit.Events
             var errors = new List<string>();
 
             // Validate logger dependency
-            var loggerField = typeof(ServiceCreatedEventHandler).GetField(
-                "_logger",
-                System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-
-            if (loggerField == null)
+            if (_loggerField is null)
             {
                 errors.Add("Logger field '_logger' not found in ServiceCreatedEventHandler.");
             }
 
             // Validate webhook handler dependency
-            var webhookHandlerField = typeof(ServiceCreatedEventHandler).GetField(
-                "_webhookHandler",
-                System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-
-            if (webhookHandlerField == null)
+            if (_webhookHandlerField is null)
             {
                 errors.Add("Webhook handler field '_webhookHandler' not found in ServiceCreatedEventHandler.");
             }
 
-            return errors.AsReadOnly();
+            return errors;
         }
 
         /// <summary>
         /// Determines whether a <see cref="ServiceCreatedEventHandler"/> instance is valid
         /// </summary>
-        /// <param name="value">The handler to check</param>
-        /// <returns>True if valid; otherwise false</returns>
-        /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null</exception>
+        /// <param name="value">The handler to check. Cannot be null.</param>
+        /// <returns>True if valid; otherwise false.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null.</exception>
         public static bool IsValid(this ServiceCreatedEventHandler value)
         {
             ArgumentNullException.ThrowIfNull(value);
@@ -64,9 +65,9 @@ namespace CaddyVpsToolkit.Events
         /// <summary>
         /// Ensures that a <see cref="ServiceCreatedEventHandler"/> instance is valid, throwing an exception if not
         /// </summary>
-        /// <param name="value">The handler to validate</param>
-        /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null</exception>
-        /// <exception cref="ArgumentException">Thrown if <paramref name="value"/> is not valid</exception>
+        /// <param name="value">The handler to validate. Cannot be null.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="value"/> is not valid.</exception>
         public static void EnsureValid(this ServiceCreatedEventHandler value)
         {
             ArgumentNullException.ThrowIfNull(value);
