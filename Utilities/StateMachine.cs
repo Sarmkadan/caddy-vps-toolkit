e087b1106d2d72c00033952584917deb21484ed8
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-23 10:26:06 +0000

    docs: add xml comments to statemachine.cs

--- Utilities/StateMachine.cs
+++ Utilities/StateMachine.cs
@@ -10,9 +10,10 @@ using System.Collections.Generic;
 namespace CaddyVpsToolkit.Utilities
 {
     /// <summary>
-    /// Simple state machine implementation with transitions and callbacks.
-    /// Useful for managing state workflows and validating allowed transitions.
+    /// Represents a simple state machine with transitions, guard clauses, and entry/exit callbacks.
     /// </summary>
+    /// <typeparam name="TState">The type representing the states.</typeparam>
+    /// <typeparam name="TTrigger">The type representing the triggers.</typeparam>
     public sealed class StateMachine<TState, TTrigger>
     {
         private TState _currentState;
@@ -22,17 +23,35 @@ namespace CaddyVpsToolkit.Utilities
         private readonly Dictionary<TState, Action> _onExitCallbacks = new();
         private Action<TState, TState, TTrigger>? _onTransitionCallback;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StateMachine{TState, TTrigger}"/> class.
+        /// </summary>
+        /// <param name="initialState">The initial state of the machine.</param>
         public StateMachine(TState initialState)
         {
             _currentState = initialState;
         }
 
+        /// <summary>
+        /// Configures a transition between states triggered by a specific trigger.
+        /// </summary>
+        /// <param name="from">The starting state.</param>
+        /// <param name="trigger">The trigger that initiates the transition.</param>
+        /// <param name="to">The resulting state.</param>
         public void Configure(TState from, TTrigger trigger, TState to)
         {
             _transitions[(from, trigger)] = to;
             _guardClauses.Remove((from, trigger));
         }
 
+        /// <summary>
+        /// Configures a transition between states triggered by a specific trigger, with a guard clause.
+        /// </summary>
+        /// <param name="from">The starting state.</param>
+        /// <param name="trigger">The trigger that initiates the transition.</param>
+        /// <param name="to">The resulting state.</param>
+        /// <param name="guardClause">A function that returns true if the transition is allowed.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="guardClause"/> is null.</exception>
         public void Configure(TState from, TTrigger trigger, TState to, Func<bool> guardClause)
         {
             ArgumentNullException.ThrowIfNull(guardClause);
@@ -41,6 +60,12 @@ namespace CaddyVpsToolkit.Utilities
             _guardClauses[(from, trigger)] = guardClause;
         }
 
+        /// <summary>
+        /// Registers a callback to be executed when entering a specific state.
+        /// </summary>
+        /// <param name="state">The state to associate the callback with.</param>
+        /// <param name="callback">The action to perform.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="callback"/> is null.</exception>
         public void OnEnter(TState state, Action callback)
         {
             ArgumentNullException.ThrowIfNull(callback);
@@ -48,6 +73,12 @@ namespace CaddyVpsToolkit.Utilities
             _onEnterCallbacks[state] = callback;
         }
 
+        /// <summary>
+        /// Registers a callback to be executed when exiting a specific state.
+        /// </summary>
+        /// <param name="state">The state to associate the callback with.</param>
+        /// <param name="callback">The action to perform.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="callback"/> is null.</exception>
         public void OnExit(TState state, Action callback)
         {
             ArgumentNullException.ThrowIfNull(callback);
@@ -55,6 +86,11 @@ namespace CaddyVpsToolkit.Utilities
             _onExitCallbacks[state] = callback;
         }
 
+        /// <summary>
+        /// Registers a callback to be executed when a transition occurs.
+        /// </summary>
+        /// <param name="callback">The action to perform, receiving previous state, current state, and trigger as parameters.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="callback"/> is null.</exception>
         public void OnTransition(Action<TState, TState, TTrigger> callback)
         {
             ArgumentNullException.ThrowIfNull(callback);
@@ -62,6 +98,11 @@ namespace CaddyVpsToolkit.Utilities
             _onTransitionCallback = callback;
         }
 
+        /// <summary>
+        /// Determines if a trigger can be fired in the current state, considering guard clauses.
+        /// </summary>
+        /// <param name="trigger">The trigger to check.</param>
+        /// <returns>True if the transition is valid; otherwise, false.</returns>
         public bool CanFire(TTrigger trigger)
         {
             if (!_transitions.ContainsKey((_currentState, trigger)))
@@ -73,6 +114,11 @@ namespace CaddyVpsToolkit.Utilities
             return true;
         }
 
+        /// <summary>
+        /// Attempts to fire a trigger, transitioning the state machine if valid.
+        /// </summary>
+        /// <param name="trigger">The trigger to fire.</param>
+        /// <returns>True if the transition was successful; otherwise, false.</returns>
         public bool Fire(TTrigger trigger)
         {
             if (!CanFire(trigger))
@@ -97,16 +143,28 @@ namespace CaddyVpsToolkit.Utilities
             return true;
         }
 
+        /// <summary>
+        /// Gets the current state of the state machine.
+        /// </summary>
+        /// <returns>The current state.</returns>
         public TState GetCurrentState()
         {
             return _currentState;
         }
 
+        /// <summary>
+        /// Manually forces the state machine to a specific state, bypassing all transitions.
+        /// </summary>
+        /// <param name="state">The target state.</param>
         public void Reset(TState state)
         {
             _currentState = state;
         }
 
+        /// <summary>
+        /// Gets a list of available triggers in the current state.
+        /// </summary>
+        /// <returns>A list of available triggers.</returns>
         public List<TTrigger> GetAvailableTransitions()
         {
             var available = new List<TTrigger>();
